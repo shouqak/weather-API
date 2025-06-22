7f5c29b28277b6b63a9fc4d65a817b65a91d3baf
shouq <shouqbis@gmail.com>
2025-06-22 20:17:30 +0300

    donee

--- src/controllers/weather.controller.ts
+++ src/controllers/weather.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express';
 import Weather from '../models/weather.model';
 import History from '../models/history.model';
 import axios from 'axios';
-import {AuthRequest} from "../middleware/auth.middleware"
-const WEATHER_API_KEY = "0954c35d618ad551c2e9a5a4b1177d67";
+import { AuthRequest } from '../middleware/auth.middleware';
+
+const WEATHER_API_KEY = '0954c35d618ad551c2e9a5a4b1177d67';
 
 const weatherApi = async (lat: number, lon: number) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
@@ -11,10 +12,12 @@ const weatherApi = async (lat: number, lon: number) => {
   return response.data;
 };
 
-export const getWeather = async (req: AuthRequest, res: Response): Promise<void> => {
+export const getWeather = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
   try {
-
-    const userId = (req as any).user.userId;
+    const userId = req.user?.userId;
 
     const { lat, lon } = req.query;
 
@@ -34,10 +37,40 @@ export const getWeather = async (req: AuthRequest, res: Response): Promise<void>
       lon: roundedLongitude,
     });
 
+    let source = 'cache';
+    let fetchedAt = new Date().toISOString();
+
     if (!weather) {
       const data = await weatherApi(latitude, longitude);
-      weather = await Weather.create({ lat: roundedLatitude, lon: roundedLongitude, data });
+
+      const formattedData = {
+        source: 'openweather',
+        coordinates: {
+          lat: latitude,
+          lon: longitude,
+        },
+        tempC: data.main.temp,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        fetchedAt,
+      };
+
+      weather = await Weather.create({
+        lat: roundedLatitude,
+        lon: roundedLongitude,
+        data: formattedData,
+      });
+
+      source = 'openweather';
     }
+
+    const weatherData = weather.data;
+
+    const finalResponse = {
+      ...weatherData,
+      source,
+      fetchedAt,
+    };
 
     await History.create({
       user: userId,
@@ -46,10 +79,10 @@ export const getWeather = async (req: AuthRequest, res: Response): Promise<void>
       lon: longitude,
     });
 
-    res.json(weather.data);
+    res.json(finalResponse);
   } catch (err: any) {
     res.status(500).json({
-      error: 'error',
+      error: 'Server error',
       details: err.message,
     });
   }
